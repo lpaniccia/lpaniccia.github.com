5b74d39618c4b199e09505d68434eeb5f9ce9e47
Lou Paniccia <loupanic@hotmail.com>
2012-12-06 21:54:48 -0500

    Displaying 3 most recent tracks
    includes album art, artist and song

--- Lastfm/script.js
+++ Lastfm/script.js
@@ -5,7 +5,11 @@ function getTracks(name) {
 	$.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=loupanic&api_key=fc8ed661e26513193e2e4732f8b7ad38&format=json", function(data){
  	 console.log(data);
 
- 	 document.getElementById("datadiv").innerHTML = data;
+ 	 var recentTracks= data;
+
+ 	 document.getElementById("datadiv1").innerHTML = '<img src="'+ recentTracks.recenttracks.track[0].image[1]["#text"] +'" style="float:left"/>' + recentTracks.recenttracks.track[0].artist["#text"] + ' <br> ' + recentTracks.recenttracks.track[0].name + '<br><br>';
+ 	 document.getElementById("datadiv2").innerHTML = '<img src="'+ recentTracks.recenttracks.track[1].image[1]["#text"] +'" style="float:left"/>' + recentTracks.recenttracks.track[1].artist["#text"] + ' <br> ' + recentTracks.recenttracks.track[1].name + '<br><br>';
+ 	 document.getElementById("datadiv3").innerHTML = '<img src="'+ recentTracks.recenttracks.track[2].image[1]["#text"] +'" style="float:left"/>' + recentTracks.recenttracks.track[2].artist["#text"] + ' <br> ' + recentTracks.recenttracks.track[2].name + '<br><br>';
 
  	 });
 
