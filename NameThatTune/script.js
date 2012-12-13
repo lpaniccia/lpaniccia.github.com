28ce034c3311209f2b961fcfa0005d8ba906cf40
Lou Paniccia <loupanic@gmail.com>
2012-12-13 14:57:22 -0500

    Added a score
    +1 for each right answer, -1 for each wrong answer.

--- NameThatTune/script.js
+++ NameThatTune/script.js
@@ -1,8 +1,17 @@
+//API settings
 var enviroment = "qa";
 var episodeID = "50ca04e4564f39df7d000002";
 var APIcall = "http://composer-"+enviroment+".ci.publicbroadcasting.net/api/episode/"+episodeID+"?part=all&offset=-5&api_key=special-key"
 
+//Declaring song array
 var answer = [ ];
+
+//Score keeping variables
+var attempt = 0;
+var rightans = 0;
+
+//Metadata on the correct song
+//TODO: This would probably work better as an array.
 var correct;
 var correctArt;
 var correctBand;
@@ -23,8 +32,11 @@ function getTracks() {
  	 //TODO: Prevent the same answer from being displayed twice.
  	 answer.push([Math.floor(Math.random()*playlistLength + 0),Math.floor(Math.random()*playlistLength + 0),Math.floor(Math.random()*playlistLength + 0),Math.floor(Math.random()*playlistLength + 0)]);
  	 
+ 	 console.log(answer);
+ 	 console.log(attempt);
+
  	 //Pick one of the 4 answers to be "correct"
- 	 correct = answer[0][Math.floor(Math.random()*3 + 0)];
+ 	 correct = answer[attempt][Math.floor(Math.random()*3 + 0)];
  	 
  	 //Store metadata on the correct song in global variables.
  	 correctArt = songs.playlist[correct].artworkUrl100;
@@ -37,10 +49,10 @@ function getTracks() {
  	 document.getElementById("player").innerHTML = '<audio controls> <source src="'+ songs.playlist[correct].previewUrl +'"" type="audio/mpeg"> </audio>';
 
  	 //Asign the answers to buttons on the page.
- 	 document.getElementById("song1").innerHTML = songs.playlist[answer[0][0]].trackName;
- 	 document.getElementById("song2").innerHTML = songs.playlist[answer[0][1]].trackName;
- 	 document.getElementById("song3").innerHTML = songs.playlist[answer[0][2]].trackName;
- 	 document.getElementById("song4").innerHTML = songs.playlist[answer[0][3]].trackName;
+ 	 document.getElementById("song1").innerHTML = songs.playlist[answer[attempt][0]].trackName;
+ 	 document.getElementById("song2").innerHTML = songs.playlist[answer[attempt][1]].trackName;
+ 	 document.getElementById("song3").innerHTML = songs.playlist[answer[attempt][2]].trackName;
+ 	 document.getElementById("song4").innerHTML = songs.playlist[answer[attempt][3]].trackName;
 
  	 //Show the buttons when DROP THE NEEDLE is clicked.
  	 document.getElementById("song1").style.visibility = "visible";
@@ -55,10 +67,14 @@ function getTracks() {
 
 function checkAnswer(guess) {
 
-	if (answer[0][guess] == correct){
-		document.getElementById("gameOver").innerHTML = '<h1 align="center">CORRECT</h1><br><img src="'+correctArt+'" style="padding:0px 10px 5px 30%; float:left"/>'+correctBand+'<br><strong>'+correctSong+'</strong><br>'+correctAlbum+'<br><a href="'+correctBuy+'" target="_blank">Buy on iTunes';
+	if (answer[attempt][guess] == correct){
+		document.getElementById("gameOver").innerHTML = '<h1 align="center">CORRECT</h1><img src="'+correctArt+'" style="padding:0px 10px 5px 30%; float:left"/>'+correctBand+'<br><strong>'+correctSong+'</strong><br>'+correctAlbum+'<br><a href="'+correctBuy+'" target="_blank">Buy on iTunes';
+		attempt ++;
+		rightans ++;
+		document.getElementById("score").innerHTML = 'Score: '+rightans;
 	} else {
 		document.getElementById("gameOver").innerHTML = '<h1 align="center">WRONG</h1>';
+		rightans --;
 	};
 
 
